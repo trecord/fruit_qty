8b26e6ee5dfe0c9caafa3e89f19c6ade4e9cd550
trecord <trevorkrecord@gmail.com>
2018-01-12 18:48:13 -0800

    Hi, anonymous dev of this fruit sorting script. I just took a quick look at your code, and was wondering if you might benefit from using a resusable sort function. Javascript allows you to set custom functions using .sort(functionName). TAke a look at the documentation: https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Array/sort
    Here's what an implementation of that for your code, let me know what you think!

--- script.js
+++ script.js
@@ -19,25 +19,14 @@ var list = [{
   'type': 'berry',
   'qty': 3
 }];
-var newlist = []
-for(var i = 0; i < list.length; i++) {
-  if (newlist.length === 0) {
-    newlist.push(list[0])
-  } else {
-    for (var j = 0; j < newlist.length; j++) {
-      if (list[i].qty < newlist[j].qty) {
-        newlist.unshift(list[i])
-        break;
-      } else if (list[i].qty > newlist[j].qty && list[i].qty <= newlist[j+1].qty) {
-        newlist.splice(j+1, 0, list[i])
-        break;
-      } else if (list[i].qty > newlist[newlist.length-1].qty) {
-        newlist.push(list[i])
-        break;
-      }
-    }
-  }
-};
+function compareQty(a,b) {
+  if (a.qty < b.qty)
+    return -1;
+  if (a.qty > b.qty)
+    return 1;
+  return 0;
+}
+var newlist = list.sort(compareQty);
 for(var i = 0; i < newlist.length; i++) {
   newlist[i] = newlist[i].name
 }
